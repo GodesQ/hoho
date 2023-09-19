c22f548691086afeef60cac80ff52ea0c22efb65
Garnfil <jamesgarnfil15@gmail.com>
2023-09-19 17:12:04 +0800

    update interests

--- app/Http/Controllers/Web/InterestController.php
+++ app/Http/Controllers/Web/InterestController.php
@@ -19,7 +19,7 @@ class InterestController extends Controller
                     ->addColumn('actions', function ($row) {
                         return '<div class="dropdown">
                                     <a href="/admin/interests/edit/' .$row->id. '" class="btn btn-outline-primary btn-sm"><i class="bx bx-edit-alt me-1"></i></a>
-                                    <a href="javascript:void(0);" class="btn btn-outline-danger remove-btn btn-sm"><i class="bx bx-trash me-1"></i></a>
+                                    <button type="button" disabled class="btn btn-outline-danger remove-btn btn-sm"><i class="bx bx-trash me-1"></i></button>
                                 </div>';
                     })
                     ->rawColumns(['actions'])
