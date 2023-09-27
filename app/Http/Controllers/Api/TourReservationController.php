ba78b10a22c712306f86c038e94f538cb166ff0e
James Garnfil <jamesgarnfil15@gmail.com>
2023-09-27 22:05:32 +0800

    remove dd() in getUserTodayReservation

--- app/Http/Controllers/Api/TourReservationController.php
+++ app/Http/Controllers/Api/TourReservationController.php
@@ -23,7 +23,7 @@ class TourReservationController extends Controller
 
     public function getUserTodayReservation(Request $request) {
         $user = Auth::user();
-        dd($user);
+        // dd($user);
         $today_date = date('Y-m-d');
         // return response($user);
         $tour_reservation = TourReservation::where('reserved_user_id', $user->id)
