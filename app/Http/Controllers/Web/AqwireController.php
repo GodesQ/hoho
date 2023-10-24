e57d9f3f4c9db2e941442e23c483ad36943ff4d2
Garnfil <jamesgarnfil15@gmail.com>
2023-10-24 13:44:51 +0800

    update aqwire controller

--- app/Http/Controllers/Web/AqwireController.php
+++ app/Http/Controllers/Web/AqwireController.php
@@ -14,7 +14,6 @@ class AqwireController extends Controller
 {
     public function success(Request $request) {
         $transaction = Transaction::where('aqwire_transactionId', $request->transactionId)->firstOrFail();
-        $reservation = TourReservation::where('order_transaction_id', $transaction->id)->firstOrFail();
 
         $update_transaction = $transaction->update([
             'aqwire_referenceId' => $request->referenceId,
@@ -23,6 +22,14 @@ class AqwireController extends Controller
             'payment_status' => Str::lower('success'),
             'payment_date' => Carbon::now()
         ]);
+
+        $reservations = TourReservation::where('order_transaction_id', $transaction->id)->get();
+
+        foreach ($reservations as $key => $reservation) {
+            $reservation->update([
+                'payment_method' => $request->paymentMethodCode
+            ]);
+        }
 
         if($update_transaction) {
             return redirect('aqwire/payment/view_success');
