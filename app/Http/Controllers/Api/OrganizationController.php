5093d78fc03e80700c485b0acf050d3d7f6e33a4
Garnfil <jamesgarnfil15@gmail.com>
2023-11-02 16:34:41 +0800

    update organization api
    - add filtered merchants base on the interests of users in single organization

--- app/Http/Controllers/Api/OrganizationController.php
+++ app/Http/Controllers/Api/OrganizationController.php
@@ -6,21 +6,59 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Organization;
+use Illuminate\Support\Facades\Auth;
 
 class OrganizationController extends Controller
-{   
-    public function getOrganizations(Request $request) {
+{
+    public function getOrganizations(Request $request)
+    {
         $organizations = Organization::get();
         return response([
             'status' => TRUE,
-           'organizations' => $organizations
+            'organizations' => $organizations
         ]);
     }
 
-    public function getOrganization(Request $request) {
-        $organization = Organization::where('id', $request->id)->with('attractions', 'stores.store_info', 'hotels.hotel_info', 'restaurants.restaurant_info', 'tour')->first();
+    public function getOrganization(Request $request)
+    {
+        // Fetch the organization data with necessary relations and filtered results
+        $organization = Organization::with([
+            'hotels.hotel_info',
+            'stores.store_info',
+            'restaurants.restaurant_info'
+        ])->where('id', $request->id)->first();
 
-        if($organization) {
+        $userInterests = Auth::user()->interest_ids ? json_decode(Auth::user()->interest_ids) : [];
+
+        // Filter and merge the results
+        $filteredMerchants = [];
+
+        // Filter hotels based on user interests and merge into the $filteredMerchants array
+        $filteredHotels = $organization->hotels->filter(function ($hotel) use ($userInterests) {
+            $hotelInterests = $hotel->hotel_info->interests;
+            return $hotelInterests && array_intersect($userInterests, json_decode($hotelInterests, true));
+        });
+        $filteredMerchants = array_merge($filteredMerchants, $filteredHotels->all());
+
+        // Filter stores based on user interests and merge into the $filteredMerchants array
+        $filteredStores = $organization->stores->filter(function ($store) use ($userInterests) {
+            $storeInterests = $store->store_info->interests;
+            return $storeInterests && array_intersect($userInterests, json_decode($storeInterests, true));
+        });
+        $filteredMerchants = array_merge($filteredMerchants, $filteredStores->all());
+
+        // Filter restaurants based on user interests and merge into the $filteredMerchants array
+        $filteredRestaurants = $organization->restaurants->filter(function ($restaurant) use ($userInterests) {
+            $restaurantInterests = $restaurant->restaurant_info->interests;
+            return $restaurantInterests && array_intersect($userInterests, json_decode($restaurantInterests, true));
+        });
+
+        $filteredMerchants = array_merge($filteredMerchants, $filteredRestaurants->all());
+
+        // Add the filtered merchants to the $organization object
+        $organization->filtered_merchants = $filteredMerchants;
+
+        if ($organization) {
             return response([
                 'status' => TRUE,
                 'organization' => $organization,
@@ -31,6 +69,5 @@ class OrganizationController extends Controller
             'status' => FALSE,
             'organization' => 'Not Found'
         ], 404);
-
     }
 }
