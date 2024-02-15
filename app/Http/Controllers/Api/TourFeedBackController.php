7ac8ccd94f6bd5cced33d647c2659b2f8fe47cdf
James Garnfil <jamesgarnfil15@gmail.com>
2024-02-15 10:25:33 +0800

    update the use of TourFeedback from TourFeedBack

--- app/Http/Controllers/Api/TourFeedBackController.php
+++ app/Http/Controllers/Api/TourFeedBackController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TourFeedback\StoreRequest;
-use App\Models\TourFeedBack;
+use App\Models\TourFeedback;
 use Illuminate\Http\Request;
 
 class TourFeedBackController extends Controller
