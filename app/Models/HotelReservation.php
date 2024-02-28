ef81f1d812db881dc4a9d0efd50c4c97d98a1a31
James Garnfil <jamesgarnfil15@gmail.com>
2024-02-28 23:05:09 +0800

    update attribute datatype of hotel reservation model

--- app/Models/HotelReservation.php
+++ app/Models/HotelReservation.php
@@ -13,6 +13,14 @@ class HotelReservation extends Model
     protected $fillable = ["reserved_user_id", "room_id", "number_of_pax", "reservation_date", "reservation_time", "status", "approved_date"];
     protected $hidden = ['created_at', 'updated_at'];
 
+    protected $casts = [
+        'reserved_user_id' => 'integer',
+        'room_id' => 'integer',
+        'number_of_pax' => 'integer',
+    ];
+
+    protected $dates = ['reservation_date', 'approved_date'];
+
     public function reserved_user() : BelongsTo {
         return $this->belongsTo(User::class, 'reserved_user_id');
     }
