6e603668012c07a7fd2f36486474500409263d4c
Garnfil <jamesgarnfil15@gmail.com>
2024-05-17 09:29:22 +0800

    update the response of getOrganizations method for API V1
    - apply organization resource for response

--- app/Http/Controllers/Api/OrganizationController.php
+++ app/Http/Controllers/Api/OrganizationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\OrganizationResource;
 use Illuminate\Http\Request;
 
 use App\Models\Organization;
@@ -15,7 +16,7 @@ class OrganizationController extends Controller
         $organizations = Organization::get();
         return response([
             'status' => TRUE,
-            'organizations' => $organizations
+            'organizations' => OrganizationResource::collection($organizations),
         ]);
     }
 
