6666dd2c8359fcfc32c0cd576214434c9920f4af
Garnfil <jamesgarnfil15@gmail.com>
2024-11-04 13:37:51 +0800

    Add food upload image in the forms of food.

--- app/Http/Requests/Food/UpdateRequest.php
+++ app/Http/Requests/Food/UpdateRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Food;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreRequest extends FormRequest
+class UpdateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,13 +24,13 @@ class StoreRequest extends FormRequest
     public function rules()
     {
         return [
-            "title" => "required",
-            "merchant_id" => "required",
-            "price" => "required",
-            "image" => "nullable|image|max:2048",
-            "food_category_id" => "required",
-            "description" => "nullable",
-            "note" => "nullable",
+            "title" => ['required'],
+            "merchant_id" => ['required'],
+            "image" => ['nullable', 'image', 'max:2048'],
+            "price" => ['required', 'numeric'],
+            "food_category_id" => ["required"],
+            "description" => ["nullable"],
+            "note" => ["nullable"],
             'other_images.*' => 'nullable|image|mimes:png,jpg,jpeg|max:2048',
         ];
     }
--- app/Models/Food.php
+++ app/Models/Food.php
@@ -9,13 +9,15 @@ class Food extends Model
 {
     use HasFactory;
     protected $table = "foods";
-    protected $fillable = ["merchant_id", "title", "description", "price", "food_category_id", "note", "is_active"];
+    protected $fillable = ["merchant_id", "title", "image", "description", "price", "food_category_id", "note", "other_images", "is_active"];
     protected $hidden = ['created_at', 'updated_at'];
-    public function merchant() {
+    public function merchant()
+    {
         return $this->belongsTo(Merchant::class, "merchant_id");
     }
 
-    public function food_category() {
+    public function food_category()
+    {
         return $this->belongsTo(FoodCategory::class, "food_category_id");
     }
 }
